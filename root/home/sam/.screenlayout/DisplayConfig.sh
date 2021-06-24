7b2186817462aa47617c9a1661fa2943a8205d81
sdauk555 <samdauk5@gmail.com>
2021-06-23 21:13:47 -0600

    idk what's in this

--- root/home/sam/.screenlayout/DisplayConfig.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI-1 --off --output DP-2 --primary --mode 1920x1080 --pos 0x220 --rotate normal
-nitrogen --restore
-killall -q plank
-plank
-sh ~/.config/polybar/launch.sh --forest
