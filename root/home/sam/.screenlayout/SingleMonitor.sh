94d0d0b82a8bd1f0278128b104905e3342559c4f
sdauk555 <samdauk5@gmail.com>
2021-07-19 13:12:23 -0600

    edited .screenlayout

---
+++ root/home/sam/.screenlayout/SingleMonitor.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VIRTUAL1 --off &
+
+nitrogen --restore &
