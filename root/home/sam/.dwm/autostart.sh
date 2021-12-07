c7e99085e63aab9bda66a76480bcd444a9c99c09
sdauk555 <samdauk5@gmail.com>
2021-12-07 11:44:34 -0700

    updated scripts

--- root/home/sam/.dwm/autostart.sh
+++ root/home/sam/.dwm/autostart.sh
@@ -1,5 +1,5 @@
 nitrogen --restore &
 picom -b &
 slstatus &
-xss-lock -- betterlockscreen -l -tf "%l:%M %_p"
+xss-lock -- betterlockscreen -l
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
--- root/home/sam/.screenlayout/SingleMonitor.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VIRTUAL1 --off &
-
-nitrogen --restore &
-sed -i 's/size: 10/size: 8/' ~/.alacritty.yml
-betterlockscreen -u ~/Pictures/wallpaper.jpeg
