a9662b153aec12f0c9b9124b93e1e50707ac1342
sdauk555 <samdauk5@gmail.com>
2022-01-09 11:27:30 -0700

    recovered scripts folder

---
+++ scripts/DualMonitor2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+xrandr --output eDP1 --off --output DP1 --primary --mode 1920x1080 --pos 0x210 --rotate normal --output HDMI1 --off --output DP2 --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI2 --off --output DP3 --off --output HDMI3 --off &
+
+nitrogen --restore &
+sed -i 's/size: 8/size: 10/' ~/.alacritty.yml
+xinput set-prop "SINOWEALTH Game Mouse" "libinput Natural Scrolling Enabled" 0
+betterlockscreen -u ~/Pictures/wallpaper.jpeg
