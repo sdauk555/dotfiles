ecdc295685ac67e4c3c82797df823a71ff0c1030
sdauk555 <samdauk5@gmail.com>
2022-07-20 16:51:53 -0600

    updated autostart

--- home/.dwm/autostart.sh
+++ home/.dwm/autostart.sh
@@ -3,11 +3,7 @@ picom -b &
 slstatus &
 dunst &
 blueman-applet &
-pa-applet &
 nm-applet &
 xss-lock -- betterlockscreen -l &
 /usr/libexec/polkit-mate-authentication-agent-1 &
 udiskie -aNT &
-sleep 2 && /home/sam/scripts/outputSwitch.sh &
-sleep 2 && mailspring &
-sleep 3 && discord &
