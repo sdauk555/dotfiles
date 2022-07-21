7b040e3f842cd9be865cfea00474bdb9a3416b8a
sdauk555 <samdauk5@gmail.com>
2022-07-20 18:31:42 -0600

    updated autostart

--- home/.dwm/autostart.sh
+++ home/.dwm/autostart.sh
@@ -4,6 +4,8 @@ slstatus &
 dunst &
 blueman-applet &
 nm-applet &
+xmodmap -e "clear Lock" -e "keycode 0x42 = Escape"
+xset dpms 0 0 1800
 xss-lock -- betterlockscreen -l &
 /usr/libexec/polkit-mate-authentication-agent-1 &
 udiskie -aNT &
