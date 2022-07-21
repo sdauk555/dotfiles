fc6c1b7cc4ce8dba6a19caaff6a1a3da67c62b18
sdauk555 <samdauk5@gmail.com>
2022-07-21 17:49:38 -0600

    updated monitor scripts

--- scripts/monitor1.sh
+++ scripts/monitor1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-xrandr --output eDP1 --off --output DP1 --primary --mode 0x542 --pos 0x0 --rotate normal
+xrandr --output eDP1 --off --output DP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 nitrogen --restore &
 sed -i 's/size: 8/size: 10/' /home/sam/.alacritty.yml
 betterlockscreen -u ~/Pictures/wallpaper.png
